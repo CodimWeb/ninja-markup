927da15a128f29a1da61341ce55e6c7b52dec496
Valeriy <codimweb@gmail.com>
2021-02-18 17:55:14 +0300

    start modal-cart

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 
 //BS4 components
 // import bootstrap from 'bootstrap';
+// import Util from 'bootstrap/js/dist/util'
+import Modal from 'bootstrap/js/dist/modal'
 
 import '../js/slick.min.js';
 import select2 from 'select2';
@@ -57,24 +59,31 @@ $(document).ready(function(){
     })
 
     // показ редактирования пиццы
-    // $('.product-pizza .product-item__picture').on('click', function(){
-    //     var offset = $(this).closest('.product-pizza').offset().top;
-    //     $('.editor').css('top', offset + 'px');
-    //     offset = offset - $('.header').outerHeight(); 
-    //     console.log(offset)
-    //     $('body,html').animate({scrollTop: offset}, 350);
+    $('.product-pizza .product-item__picture').on('click', function(){
+        var offset = $(this).closest('.product-pizza').offset().top;
+        $('.editor').css('top', offset + 'px');
+        offset = offset - $('.header').outerHeight();
+        var currScroll =  $(window).scrollTop();
+        var scrollLenght = Math.ceil(Math.abs(offset - currScroll));
+        var animationTime = scrollLenght * 1.2;
 
-    //     setTimeout(() => {
-    //         document.body.style.overflow = 'hidden';
-    //     }, 400);
-        
-    // })
+        // console.log(offset)
+        // console.log($(window).scrollTop(), 'scroll');
+        // console.log(scrollLenght)
+        $('body,html').animate({scrollTop: offset}, animationTime).promise().then(function() {
+            console.log('end animate');
+            var scrollBarWidth = getScrollbarWidth()
+            document.body.style.paddingRight = scrollBarWidth + 'px';
+            document.body.style.overflow = 'hidden';
+            $('.editor').addClass('open')
+        });
+    })
 
-    // скрытие редактирования пиццы
-    // $('.btn-close-editor').on('click', function(){
-    //     $('.editor').fadeOut(300);
-    //     document.body.removeAttribute('style');
-    // })
+    //скрытие редактирования пиццы
+    $('.btn-close-editor').on('click', function(){
+        $('.editor').removeClass('open');
+        document.body.removeAttribute('style');
+    })
      
 
     $('#editor-sauces').select2({
@@ -83,19 +92,36 @@ $(document).ready(function(){
         dropdownCssClass: 'editor-select__dropdown'
     });
 
-    $('.editor').fadeIn(300, function() {
-        console.log('asdasd')
-        $('.editor-slider .slick-slider').slick({
-            dots: false,
-            arrows: true,
-            autoplay: false,
-            speed: 300,
-            infinite: false,
-            slidesToScroll: 1,
-            slidesToShow: 2,
-            prevArrow: $('.editor-slider-prev'),
-            nextArrow: $('.editor-slider-next'),
-        });
+    $('.editor-slider .slick-slider').slick({
+        dots: false,
+        arrows: true,
+        autoplay: false,
+        draggable: false,
+        speed: 300,
+        infinite: false,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        variableWidth: true,
+        prevArrow: $('.editor-slider-prev'),
+        nextArrow: $('.editor-slider-next'),
     });
+
+    $('.editor-slider__item').on('click', function(){
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active')
+        } else {
+            $(this).addClass('active')
+        }
+    })
 });
+
+function getScrollbarWidth() { 
+    var scrollDiv = document.createElement('div')
+    scrollDiv.className = 'modal-scrollbar-measure';
+    document.body.appendChild(scrollDiv)
+    var scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth
+    document.body.removeChild(scrollDiv)
+    console.log(scrollbarWidth);
+    return scrollbarWidth
+  }
 
